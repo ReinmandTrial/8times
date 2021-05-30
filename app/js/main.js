0babfe2604baacf16f97519f919d2e4931766464
kirill.pr9n1k <kirill.pr9n1k@yandex.ru>
2021-05-30 22:51:24 +0300

    смена цвета и селект

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,35 @@
-const { default: Swiper } = require("swiper");
+// const { default: Swiper } = require("swiper");
 
-$(function () {
+// $(function () {
 
-});
-new Swiper('.container', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    slidesPerView: 5,
-});
+// });
+// new Swiper('.container', {
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//     },
+//     slidesPerView: 5,
+// });
+//выбор цвета
+$('.page-product__color').on('click',function(){
+    var item = $(this).closest('.page-product__color-change-box').find('.page-product__color');
+    $(item).each(function(){
+        $(this).removeClass('active');
+    })
+    $(this).addClass('active');
+})
+//выбор цвета конец
+//выподалка размеров
+$('.page-product__size-header').on('click',function(){
+    $(this).next().slideToggle('slow');
+})
+
+$('.page-product__size-link').on('click',function(){
+    var size = $(this).find('.page-product__size-num').text();
+    $(this).closest('.page-product__size-box').find('.page-product__size-title').text('Размер ' + size);
+    $(this).closest('.page-product__size-body').slideToggle('slow');
+})
+//выподалка размеров конец
+//burger menu 
+
+//burger menu end
