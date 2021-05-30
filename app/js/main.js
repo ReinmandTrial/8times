8579556d6663293cfd3aa822a9ed2c7b8f84b945
spjustice <sanyok.vergun@gmail.com>
2021-05-31 02:39:04 +0300

    Иконки настроены, комментарии проставлены, слайдер не сделан

--- app/js/main.js
+++ app/js/main.js
@@ -3,28 +3,32 @@
 // $(function () {
 
 // });
-// new Swiper('.container', {
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev'
-//     },
-//     slidesPerView: 5,
-// });
+new Swiper('.swiper-slide', {
+    navigation: {
+        nextE: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: auto,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true
+    },
+});
 //выбор цвета
-$('.page-product__color').on('click',function(){
+$('.page-product__color').on('click', function () {
     var item = $(this).closest('.page-product__color-change-box').find('.page-product__color');
-    $(item).each(function(){
+    $(item).each(function () {
         $(this).removeClass('active');
     })
     $(this).addClass('active');
 })
 //выбор цвета конец
 //выподалка размеров
-$('.page-product__size-header').on('click',function(){
+$('.page-product__size-header').on('click', function () {
     $(this).next().slideToggle('slow');
 })
 
-$('.page-product__size-link').on('click',function(){
+$('.page-product__size-link').on('click', function () {
     var size = $(this).find('.page-product__size-num').text();
     $(this).closest('.page-product__size-box').find('.page-product__size-title').text('Размер ' + size);
     $(this).closest('.page-product__size-body').slideToggle('slow');
