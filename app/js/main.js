d2e2cbc311909d0e46e6daf9aa2f32a710abe6b5
spjustice <sanyok.vergun@gmail.com>
2021-05-30 19:35:19 +0300

    0.97

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
+const { default: Swiper } = require("swiper");
+
 $(function () {
 
-})
+});
+new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.products-row__button',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 5,
+});
