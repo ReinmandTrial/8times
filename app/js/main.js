c0bad651b5cbf51688acd3ad045e64ccb64e08e4
spjustice <sanyok.vergun@gmail.com>
2021-06-01 00:03:42 +0300

    Сердца поверх картинки сделал. Слайдер починил. Начал делать страницу корзины^C

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,9 @@
-$($('.page-main__container').find('.products-row__container')).each(function(){
-    new Swiper('.swiper-container', {
+
+new Swiper('.swiper-container', {
     // навигация, кнопки
     navigation: {
-        nextEl: '.products-row__button_next',
-        prevEl: '.products-row__button_prev',
+        nextEl: '.products-slider__button_next',
+        prevEl: '.products-slider__button_prev',
     },
     // модификации 
     grabCursor: true, //  курсор-рука
@@ -13,23 +13,19 @@ $($('.page-main__container').find('.products-row__container')).each(function(){
     spaceBetween: 20, // отступы между слайдами
     loop: true, // бесконечный слайдер
     slideToClickedSlide: false,
+    breakpoints: {
+        0: {
+            spaceBetween: 4,
+        },
+        768: {
+            spaceBetween: 20,
+            slidesPerView: '4',
+        },
+        1200: {
+            slidesPerView: '5',
+        },
+    },
 });
-})
-// new Swiper('.swiper-container', {
-//     // навигация, кнопки
-//     navigation: {
-//         nextEl: '.products-row__button_next',
-//         prevEl: '.products-row__button_prev',
-//     },
-//     // модификации 
-//     grabCursor: true, //  курсор-рука
-//     autoHeight: true, // автовысота
-//     slidesPerView: 'auto', //кол-во показываемых слайдов
-//     watchOverflow: true, //если слайдов мало, - слайдер отключается
-//     spaceBetween: 20, // отступы между слайдами
-//     loop: true, // бесконечный слайдер
-//     slideToClickedSlide: false,
-// });
 //выбор цвета
 $('.page-product__color').on('click', function () {
     var item = $(this).closest('.page-product__color-change-box').find('.page-product__color');
@@ -55,10 +51,10 @@ $('.page-product__size-link').on('click', function () {
 //burger menu end\
 
 //регистрация попап
-$('.btn-reg').on('click',function(){
+$('.btn-reg').on('click', function () {
     $('.page-reg').fadeIn();
 })
-$('.reg__form>.close').on('click',function(){
+$('.reg__form>.close').on('click', function () {
     $(this).closest('.page-reg').fadeOut();
 })
 //регистрация попап конец
